b867b76294958dc24cd4858bb2d9f3f65408871c
Depei Zhu <hyekyoz@outlook.com>
2019-01-05 01:02:16 +0800

    user name and avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -5,15 +7,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[github]
 
+  has_one_attached :avatar
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      # user.name = auth.info.name   # assuming the user model has a name
-      # user.avatar = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails,
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
+      user.email        = auth.info.email
+      user.password     = Devise.friendly_token[0, 20]
+      user.name         = auth.info.name # assuming the user model has a name
+      downloaded_avatar = open(auth.info.image)
+      user.avatar.attach(io: downloaded_avatar, filename: 'avatar')
     end
   end
 end
