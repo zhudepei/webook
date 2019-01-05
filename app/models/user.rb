1e6e2fba148c3693fb13ad98c969ec382c689bf9
Depei Zhu <hyekyoz@outlook.com>
2019-01-05 13:56:21 +0800

    config database.yml

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
+         :trackable, :lockable, :timeoutable,
          :omniauthable, omniauth_providers: %i[github]
 
   has_one_attached :avatar
